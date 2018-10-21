3a3b76d569167349d793d165f294d31800e4ca7a
Fedor Paretsky <me@f3d0r.com>
2018-10-21 08:06:27 -0700

    Remove deprecated storage reference from multer

--- routes/api/main.js
+++ routes/api/main.js
@@ -1,14 +1,6 @@
 var router = require('express').Router();
 var multer = require('multer');
-var sharp = require('sharp');
 var fs = require('fs');
-var uniqueString = require('unique-string');
-const constants = require('@config');
-var sql = require('@sql');
-
-var upload = multer({
-    storage: storage
-});
 
 router.get('/', function (req, res, next) {
     res.status(200).send("Welcome to the main endpoint!");
