2ee53cd399860212917bff5d33258dd60a221d3c
Fedor Paretsky <me@f3d0r.com>
2018-10-21 13:07:50 -0700

    Add python keras req

--- routes/api/main.js
+++ routes/api/main.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 var fs = require('fs');
 var cmd = require('node-cmd');
+const exec = require('child_process').exec;
 
 router.get('/', function (req, res) {
     res.status(200).send("Welcome to the main endpoint!");
@@ -17,19 +18,36 @@ router.get('/run_keras', function (req, res) {
             res.status(500).send("ERROR WRITING FILE!");
         } else {
             cmd.get(
-                'python3 /home/fedor/json_to_hdf5.py',
+                'cd /home/fedor && python3 /home/fedor/json_to_hdf5.py',
                 function (err, data, stderr) {
                     if (err) {
                         res.status(500).send("ERROR: " + err);
                     } else {
-                        res.send("output: " + stderr);
+                        os.execCommand('cd /home/fedor && python3 /home/fedor/hdf5_keras.py').then(resOut => {
+                            res.send("output: " + resOut)
+                        }).catch(err => {
+                            console.log("os >>>", err);
+                        })
                     }
-
                 }
             );
         }
-
     });
 });
+
+function os_func() {
+    this.execCommand = function (cmd) {
+        return new Promise((resolve, reject) => {
+            exec(cmd, (error, stdout, stderr) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                resolve(stdout)
+            });
+        })
+    }
+}
+var os = new os_func();
 
 module.exports = router;
