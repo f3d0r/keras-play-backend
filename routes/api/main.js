650c9db70ec01c4400911f44b354d30ac680d283
Fedor Paretsky <me@f3d0r.com>
2018-10-21 09:15:05 -0700

    Add node-cmd interface/package

--- routes/api/main.js
+++ routes/api/main.js
@@ -2,12 +2,16 @@ var router = require('express').Router();
 var multer = require('multer');
 var fs = require('fs');
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.status(200).send("Welcome to the main endpoint!");
 });
 
-router.get('/ping', function (req, res, next) {
+router.get('/ping', function (req, res) {
     res.status(200).send("pong");
 });
+
+router.get('/run_keras', function(req, res)) {
+    
+}
 
 module.exports = router;
