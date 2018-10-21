b1e587f18ba8d06fafc61f1dc67014ebddcb63b3
Fedor Paretsky <me@f3d0r.com>
2018-10-21 10:02:49 -0700

    Add node-cmd integration

--- routes/api/main.js
+++ routes/api/main.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 var multer = require('multer');
 var fs = require('fs');
+var cmd = require('node-cmd');
 
 router.get('/', function (req, res) {
     res.status(200).send("Welcome to the main endpoint!");
@@ -10,8 +11,26 @@ router.get('/ping', function (req, res) {
     res.status(200).send("pong");
 });
 
-router.get('/run_keras', function(req, res) {
-    res.send(req.body);
+router.get('/run_keras', function (req, res) {
+    var body = JSON.stringify(req.body);
+    fs.writeFile('/home/fedor/current_body.json', body, (err) => {
+        if (err) {
+            res.status(500).send("ERROR WRITING FILE!");
+        } else {
+            cmd.get(
+                'python3 /home/fedor/json_to_hdf5.py',
+                function (err, data, stderr) {
+                    if (err) {
+                        res.status(500).send("ERROR: " + err);
+                    } else {
+                        res.send("output: " + stderr);
+                    }
+
+                }
+            );
+        }
+
+    });
 });
 
 module.exports = router;
